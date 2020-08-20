6c38f1a41a2407955dd8f90bc567b49a110b6ec4
刁琪 <diaoqi@diaoqi.local>
2020-08-20 16:26:56 +0800

    内嵌页调原生登录

--- src/utils/login.js
+++ src/utils/login.js
@@ -3,6 +3,7 @@
  * @Date: 2020-08-17 13:45:23
  * @LastEditors: わからないよう
  */
+/* eslint-disable no-undef */
 import Taro from '@tarojs/taro'
 import { getCookie, setCookie } from './cookie'
 
@@ -13,9 +14,15 @@ export function hasUserState() {
     if (token) {
       resolve(true);
     } else {
-      Taro.navigateTo({
-        url: `/pages/user/login/login`,
-      });
+      if (navigator.userAgent.includes('TZGCanadaApp')) {
+        // 加拿大APP内嵌页面 调原生登录
+        ToFlutterIsNotLogin.postMessage('NotLogin')
+      } else {
+        // H5
+        Taro.navigateTo({
+          url: `/pages/user/login/login`,
+        });
+      }
       resolve(false);
     }
   });
